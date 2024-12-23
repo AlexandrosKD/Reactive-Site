b287f9bf26d4a8340ec787bd667b51384a72575d
Alexandros Kesidis <alexandroskesidis12@gmail.com>
2024-12-23 17:29:19 +0200

    First commit

---
+++ src/pages/home.jsx
@@ -0,0 +1,16 @@
+import "../styles/pages/home.css";
+
+export default function Home() {
+  return (
+    <div className="taskbar">
+      <ul>
+        <li>
+          <a>Home</a>
+        </li>
+        <li>
+          <a>Contact</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
