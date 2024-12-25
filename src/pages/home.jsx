259d3b8edec991f641a511129d54ef8db604645d
AlexandrosKD <alexandroskesidis12@gmail.com>
2024-12-26 00:39:56 +0200

    Added pseudo-Dark theme

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,14 +1,40 @@
 import "../styles/pages/home.css";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { motion } from "motion/react";
+import { useState } from "react";
+import { BsSun, BsFillMoonStarsFill } from "react-icons/bs";
+
+const Sidebar = () => {};
 
 export default function Home() {
+  const [isDark, setIsDark] = useState(false);
+
+  const toggleOn = () => {
+    setIsDark(!isDark);
+  };
+
   return (
     <div className="taskbar">
       <ul>
         <li>
-          <a>Home</a>
+          <motion.button
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.95 }}
+            type="button"
+            className="hmbgrBtn"
+          >
+            <GiHamburgerMenu size={24} />
+          </motion.button>
         </li>
         <li>
-          <a>Contact</a>
+          <motion.button
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.95 }}
+            className="hmbgrBtn"
+            onClick={toggleOn}
+          >
+            {isDark ? <BsSun size={24} /> : <BsFillMoonStarsFill size={24} />}
+          </motion.button>
         </li>
       </ul>
     </div>
