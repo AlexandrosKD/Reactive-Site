f377032c015ce821c53b585d52afd5cbc6c6623f
AlexandrosKD <alexandroskesidis12@gmail.com>
2025-01-01 01:37:47 +0200

    Finally Centered Text

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,18 @@
 import RootLayout from "../layouts/rootLayouts";
 import React from "react";
+import "../styles/pages/home.css";
 
 export default function Home() {
   return (
     <RootLayout>
-      <p>Homer Simpson</p>
+      <div className="introduce">
+        Hello! My name is Alexandros Kesidis.
+        <br />
+        <p className="introduceParagraph">
+          I'm a junior programmer. One of the languages I'm learning is Java,
+          with projects like:
+        </p>
+      </div>
     </RootLayout>
   );
 }
